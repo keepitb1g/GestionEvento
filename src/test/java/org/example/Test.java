b8d2fcc039659f380200fb683c221d9d34d0a72b
Joaquin <j.villa04@ufromail.cl>
2024-09-01 18:35:45 -0400

    Primer commit, creación del repositorio

---
+++ src/test/java/org/example/Test.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void crearLista() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void agregarPersona() {
+    }
+}
